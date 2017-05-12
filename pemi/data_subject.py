f72329911190e12e406f15e1d4b43fc06a55cf0e
Sterling Paramore <sterling.paramore@insidetrack.com>
2017-05-11 20:33:23 -0700

    Working data target!
    I revised the idea of an interface because I thought the
    number of engines would explode and be difficult to manage
    converting between them.
    
    So, "engines" are now core data structures.  There should
    be a smaller number of them (e.g., pandas, spark, elt) and
    allowed conversions between them should be clearly identified.
    
    There will be many more "interfaces".  Interfaces are the things
    between source/targets and the engine.  Examples include
    csv files, parquet files, dictionary of arrays, array of
    dictionaries.  The interfaces are expected to know how
    to convert to the core engines.
    
    There will be even more data source and targets.
    Data sources extract data from some place and load it into an
    interface.  For example, an SFTP, local filesystem, or S3
    could download csv files and pass those to a CSV interface.
    Or, a REST API source could collect responses as an array
    of dictionaries.  These results are then passed to a
    supported interface.  Data targets work the same way,
    but in reverse.
    
    So a typical data pipeline then proceeds as:
    DataSource
    -> Interface
    -> Engine
    -> various transformations
    -> Engine
    -> Interface
    -> DataTarget

--- pemi/data_subject.py
+++ pemi/data_subject.py
@@ -1,69 +1,61 @@
 import pandas as pd
+import pemi
 from pemi.decorators import cached_property
 
 class DataSubject():
     '''
-    A data subject is a parent class defined by a schema and engine class.
+    A data subject is a parent class defined by a schema and interface class.
     '''
 
-    def __init__(self, schema, engine_class):
+    def __init__(self, schema, interface_class):
         self.schema = schema
-        self.engine_class = engine_class
+        self.interface_class = interface_class
 
     @cached_property
-    def engine(self):
+    def interface(self):
         '''
-        The engine is constructed in a specific child class and describes
-        how to get elements of data.
+        The interface is constructed in a specific child class and describes
+        how to interact with elements of data.
         '''
+        return self.build_interface()
+
+    def build_interface(self):
+        ''' DataSources use the extracted data to build an interface '''
         raise NotImplementedError
 
+# should every interface define a default engine?
     def __getitem__(self, key):
-        ''' DataSubjects wrap access to data stored in the engine '''
+        ''' DataSubjects wrap access to data stored in an engine constructed by the interface '''
         return self.engine[key]
 
 class DataSource(DataSubject):
     '''
-    Data sources extract data from some thing and build the engine.
+    Data sources extract data from some interface and build the engine.
     '''
-
-    @cached_property
-    def engine(self):
-        return self.build_engine()
-
-    def build_engine(self):
-        ''' DataSources use the extracted data to build an engine '''
-        raise NotImplementedError
 
     # DataSources convert the data they have extracted to other engines
     def to_pandas(self):
-        return self.engine.to_pandas()
+        return self.interface.to_pandas()
 
     def to_spark(self):
-        return self.engine.to_spark()
+        return self.interface.to_spark()
 
     def to_alchemy(self):
-        return self.engine.to_alchemy()
+        return self.interface.to_alchemy()
 
 
 class DataTarget(DataSubject):
-    @cached_property
-    def engine(self):
-        '''
-        The engine is constructed in a specific child class and describes
-        how to get elements of data.
-        '''
-        raise AttributeError('Engine has not been defined')
+    '''
+    Data targets import data from an engine and load it via an interface
+    '''
 
-    # DataTargets read data from some type of engine
-    def from_pandas(self, df):
-        self.engine = PandasEngine(df).to_engine(self.engine_class)
-
-    def from_spark(self, df):
-        self.engine = SparkEngine(df).to_engine(self.engine_class)
-
-    def from_alchemy(self, df):
-        self.engine = AlchemyEngine(df).to_engine(self.engine_class)
+    def from_engine(self, engine):
+        if engine.__class__ == pemi.engines.PandasEngine:
+            self.interface.from_pandas(engine)
+#        elif engine.__class__ == SparkEngine:
+#            self.interface.from_spark(engine)
+        else:
+            raise NotImplementedError('Engine {} not recognized'.format(engine.__class__))
 
     def encode(self):
         ''' DataTargets encode the engine into a form that the target system can understand '''
---
+++ pemi/interfaces/__init__.py
@@ -0,0 +1,1 @@
+from pemi.interfaces.interface import Interface
