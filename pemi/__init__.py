a1259a7ebd564b504cbb5797282ba4aa271deb04
Sterling Paramore <sterling.paramore@insidetrack.com>
2017-09-26 17:10:02 -0700

    Make pemi modules easier to select

--- pemi/__init__.py
+++ pemi/__init__.py
@@ -9,7 +9,16 @@ logging.getLogger('pemi').setLevel(logging.WARN)
 def log(name='pemi'):
     return logging.getLogger(name)
 
-
+import pemi.schema
 from pemi.schema import Schema
 
+import pemi.pipe
 from pemi.pipe import Pipe
+
+import pemi.fields
+from pemi.fields import *
+
+import pemi.data_subject
+from pemi.data_subject import *
+
+import pemi.transforms
