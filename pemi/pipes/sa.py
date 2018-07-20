6b65ba3242536c3eb88d2f735cac5f847126ffb1
Sterling Paramore <sterling.paramore@insidetrack.com>
2018-07-20 13:54:51 -0700

    Allows you to run SQL or parse a CSV without a schema
    Good for quick exploratory work.

--- pemi/pipes/sa.py
+++ pemi/pipes/sa.py
@@ -1,38 +1,60 @@
 import pandas as pd
 
 import pemi
-import pemi.pipes.patterns
 
-class SaSqlSourcePipe(pemi.pipes.patterns.SourcePipe):
-    def __init__(self, *, sql, engine, **params):
-        super().__init__(**params)
+class SaSqlSourcePipe(pemi.Pipe):
+    def __init__(self, *, sql, engine, schema=None, result=True, chunk_size=None):
+        super().__init__()
 
         self.sql = sql
         self.engine = engine
+        self.schema = schema
+        self.result = result
+        self.chunk_size = chunk_size
+
+        self.target(
+            pemi.PdDataSubject,
+            name='main',
+            schema=self.schema
+        )
+
+
+    def _get_result(self, conn):
+        sql_df = pd.DataFrame()
+        if self.chunk_size is None:
+            sql_df = pd.read_sql(self.sql, conn)
+        else:
+            for chunk in pd.read_sql(self.sql, conn, chunksize=self.chunk_size):
+                sql_df = sql_df.append(chunk, ignore_index=True)
+        return sql_df
+
 
     def extract(self):
-        pemi.log.info("Extracting '%s' via:\n%s", self.name, self.sql)
-        chunk_size = self.params['chunksize'] if 'chunksize' in self.params else None
-        sql_df = pd.DataFrame()
-        with self.engine.connect() as conn:
-            if chunk_size is None:
-                sql_df = pd.read_sql(self.sql, conn)
-            else:
-                for chunk in pd.read_sql(self.sql, conn, chunksize=chunk_size):
-                    sql_df = sql_df.append(chunk, ignore_index=True)
+        pemi.log.info("Executing SQL '%s' via:\n%s", self.name, self.sql)
 
-        return sql_df
+        data = None
+        with self.engine.connect() as conn:
+            if self.result:
+                data = self._get_result(conn)
+            else:
+                conn.execute(self.sql)
+
+        return data
 
     def parse(self, data):
         pemi.log.info("Parsing '%s' results", self.name)
 
-        mapper = data.mapping(
-            [(name, name, field.coerce) for name, field in self.schema.items()],
-            on_error='raise'
-        )
+        if data is None:
+            return None
+        elif self.schema is None:
+            self.targets['main'].df = data
+        else:
+            mapper = data.mapping(
+                [(name, name, field.coerce) for name, field in self.schema.items()],
+                on_error='raise'
+            )
+            self.targets['main'].df = mapper.mapped
 
-        self.targets['main'].df = mapper.mapped
-        self.targets['errors'].df = mapper.errors
         return self.targets['main'].df
 
     def flow(self):
