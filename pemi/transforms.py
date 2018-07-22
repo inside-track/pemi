27fd096d9641971f34cb0811fc2240ebc4f3450b
Mostafa Fotoh <mosfotoh@gmail.com>
2018-07-22 17:24:57 +0200

    DE-1903 - fix isblank() error

--- pemi/transforms.py
+++ pemi/transforms.py
@@ -1,19 +1,21 @@
-import pandas as pd
-
 def isblank(value):
     return (
-        value is not False
-        and value != 0
-        and value != float(0)
-        and (value is None or pd.isnull(value) or not value)
+            value is not False
+            and value != 0
+            and value != float(0)
+            and not bool(value)
     )
+
 
 def concatenate(delimiter=''):
     def _concatenate(row):
         return delimiter.join(row)
+
     return _concatenate
+
 
 def nvl(default=''):
     def _nvl(row):
         return next((v for v in row if not isblank(v)), default)
+
     return _nvl
