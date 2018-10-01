2fafc189c584b3bef14d24e9b2bf01acd17330d7
Sterling Paramore <sterling.paramore@insidetrack.com>
2018-10-01 16:47:16 -0700

    Add some missing dependencies for PemiTabular

--- setup.py
+++ setup.py
@@ -64,8 +64,7 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
@@ -88,7 +87,8 @@ setup(
         'dask>=0.18',
         'graphviz>=0.8',
         'faker>=0.8',
-        'pandas-mapper>=0.1.0'
+        'pandas-mapper>=0.1.0',
+        'tabulate>=0.8'
     ],
 
     python_requires='>=3',
